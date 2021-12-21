e704c8bcae3c5db1345c2473a6c120cbe6369aee
karlengevorgyan_qt <gevorgyankarlen@gmail.com>
2021-12-21 04:10:09 -0800

    Add test cases

---
+++ src/test/java/advent_of_code/day1/SonarSweepPart2Test.java
@@ -0,0 +1,45 @@
+package advent_of_code.day1;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SonarSweepPart2Test {
+    @Test
+    @DisplayName("The count of depth increases must be 5, when the floor of the sea is uneven")
+    public void count_of_depth_increases_must_be_5() {
+        var listOfDepth = List.of(199, 200, 208, 210, 200, 207, 240, 269, 260, 263);
+        var numberOfDepth = SonarSweep.count_of_depth_increases_part2(listOfDepth);
+
+        assertEquals(5, numberOfDepth);
+    }
+
+    @Test
+    @DisplayName("There should be no increases, when the floor of the sea is level")
+    public void count_of_depth_increases_must_be_0_when_floor_is_level() {
+        var listOfDepth = List.of(199, 199, 199, 199, 199);
+        var numberOfDepth = SonarSweep.count_of_depth_increases_part2(listOfDepth);
+
+        assertEquals(0, numberOfDepth);
+    }
+
+    @Test
+    @DisplayName("There should be no increases, when the floor of  the sea is inclining")
+    public void count_of_depth_increases_must_be_0_when_floor_go_up() {
+        var listOfDepth = List.of(199, 198, 197, 180, 170);
+        var numberOfDepth = SonarSweep.count_of_depth_increases_part2(listOfDepth);
+
+        assertEquals(0, numberOfDepth);
+    }
+
+    @Test
+    @DisplayName("The count of depth increases must be 2, when the floor of  the sea is declining")
+    public void count_of_depth_increases_must_be_2_when_floor_go_down() {
+        var listOfDepth = List.of(199, 200, 240, 260, 300);
+        var numberOfDepth = SonarSweep.count_of_depth_increases_part2(listOfDepth);
+
+        assertEquals(2, numberOfDepth);
+    }
+}
