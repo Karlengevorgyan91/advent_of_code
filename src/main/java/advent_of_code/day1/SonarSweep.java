a9e388fc96ff554e5ec1b65ebbb00a37ccaeb51a
karlengevorgyan_qt <gevorgyankarlen@gmail.com>
2021-12-21 04:08:50 -0800

    Changes in classes `SonarSweep` and `SonarSweepTest`

--- src/main/java/advent_of_code/day1/SonarSweep.java
+++ src/main/java/advent_of_code/day1/SonarSweep.java
@@ -11,12 +11,23 @@ public class SonarSweep {
         var path = "src/main/java/advent_of_code/day1/input";
         var listOfLines = Util.getFileLines(path);
         var listOfNumbers = parsToInt(listOfLines);
-        var countOfDepth = count_of_depth_increases(listOfNumbers);
+        var countOfDepthPart1 = count_of_depth_increases_part1(listOfNumbers);
+        var countOfDepthPart2 = count_of_depth_increases_part2(listOfNumbers);
 
-        System.out.println(countOfDepth);
+        System.out.println(countOfDepthPart1);
+        System.out.println(countOfDepthPart2);
     }
 
-    public static Integer count_of_depth_increases(List<Integer> depthReading) {
+    public static Integer count_of_depth_increases_part2(List<Integer> depthReading) {
+        var listOfSum = Util.sliding(depthReading, 3).stream()
+                .map(element -> element.get(0) + element.get(1) + element.get(2)).collect(Collectors.toList());
+
+        return Util.sliding(listOfSum, 2).stream()
+                .map(element -> element.get(1) - element.get(0))
+                .filter(number -> number > 0).collect(Collectors.toList()).size();
+    }
+
+    public static Integer count_of_depth_increases_part1(List<Integer> depthReading) {
         return Util.sliding(depthReading, 2).stream()
                 .map(element -> element.get(1) - element.get(0))
                 .filter(number -> number > 0).collect(Collectors.toList()).size();
